94473d5f6c890a37d43c4582e8637502bd69db98
afzal mahmud <afzzalmahmud@gmail.com>
2023-08-20 15:33:20 +0600

    [Mobile-menu] implemented mobile responsive menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import { BsFillCartFill } from "react-icons/bs";
+import { MdFavorite,MdHelp } from "react-icons/md";
+import { FaUserFriends, FaWallet } from "react-icons/fa";
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       {/* Left side */}
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div onClick={() => setNav(!nav)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
@@ -26,6 +31,31 @@ const Navbar = () => {
       <button className="bg-black text-white flex items-center py-2 rounded-full">
         Log In
       </button>
+       {/* Mobile Menu */}
+      {/* Overlay */}
+      {nav ? <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div> : ''}
+      
+
+      {/* Side drawer menu */}
+      <div className={nav ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300' : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300' }>
+        <AiOutlineClose
+            onClick={()=> setNav(!nav)}
+          size={30}
+          className='absolute right-4 top-4 cursor-pointer'
+        />
+        <h2 className='text-2xl p-4'>
+          Gray <span className='font-bold'>Books</span>
+        </h2>
+        <nav>
+            <ul className='flex flex-col p-4 text-gray-800'>
+                <li className='text-xl py-4 flex'><BsFillCartFill size={25} className='mr-4' /> Carts</li>
+                <li className='text-xl py-4 flex'><MdFavorite size={25} className='mr-4' /> Wishlist </li>
+                <li className='text-xl py-4 flex'><FaWallet size={25} className='mr-4' /> Wallet</li>
+                <li className='text-xl py-4 flex'><MdHelp size={25} className='mr-4' /> Help</li>
+                <li className='text-xl py-4 flex'><FaUserFriends size={25} className='mr-4' /> Invite Friends</li>
+            </ul>
+        </nav>
+      </div>
     </div>
   );
 };
