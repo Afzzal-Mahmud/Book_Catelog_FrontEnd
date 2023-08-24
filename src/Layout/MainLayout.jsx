3296c251e8c98dc1ec2b436d6f34f13be284273c
afzal mahmud <afzzalmahmud@gmail.com>
2023-08-24 23:58:43 +0600

    [Layout] implemented main layout

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+export default function MainLayout() {
+  return (
+    <div>
+      <Navbar />
+      <div className="pt-16">
+        <Outlet />
+      </div>
+      <Footer/>
+    </div>
+  );
+}
